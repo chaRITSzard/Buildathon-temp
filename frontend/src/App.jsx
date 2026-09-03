1d36a911de06c16f38e70be7cc5b1d9e104a29d8
ritvik <ritviksny@gmail.com>
2026-09-04 01:57:16 +0530

    feat: changes for the Render deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header'
+import Header from './components/header'
 import QuestionBar from './components/QuestionBar'
 import ResponsePanel from './components/ResponsePanel'
 import ActionCard from './components/ActionCard'
